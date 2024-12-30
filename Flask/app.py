3b5e7e1971da853b71ea136bc194130834fe4319
Oleh_Lambda <olegzsybulskyi2004@gmail.com>
2024-12-30 16:09:26 +0200

    First flask app

--- Flask/app.py
+++ Flask/app.py
@@ -2,4 +2,13 @@
 # Це мікрофреймворк для Python, який дозволяє створювати веб-додатки.
 
 # - Встановлення Flask -
-# pip install Flask
+# pip install flask
+
+# - Створення першого веб-додатку -
+import flask
+app = flask.Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello, World!"
+if __name__ == "__main__":
+    app.run()
